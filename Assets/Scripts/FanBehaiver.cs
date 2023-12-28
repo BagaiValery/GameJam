6040fc644e5bfd8bb841d7eb6caf2c862ffaad9c
Bagay Valery <lerika1307@gmail.com>
2023-12-28 13:00:54 +0300

    Add tag for Player and make automatycale choice "star"

--- Assets/Scripts/FanBehaiver.cs
+++ Assets/Scripts/FanBehaiver.cs
@@ -10,7 +10,7 @@ public class FanBehaiver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Star").GetComponent<Transform>();
     }
 
     // Update is called once per frame
